33f9ecd8486eab1bb6cae8e7f0f875cdbcd376ec
aankit <aankit.patel@gmail.com>
2020-07-08 16:37:20 -0400

    third query written

--- query.py
+++ query.py
@@ -4,6 +4,9 @@ from parse import parse_ceps
 import csv
 import re
 from fuzzysearch import find_near_matches
+import pprint
+
+pp = pprint.PrettyPrinter()
 
 
 #add term as a key and count as value to each record
@@ -21,46 +24,51 @@ def add_count_term_to_record(records, term, case_senstive=False):
 
 
 def create_new_record_each_term(records, term, case_senstive=False):
+    excerpt_range = 30
     term_records = []
     for record in records:
         if case_senstive:
-            for match in re.finditer(rf'{term}', record['answer']):
-                new_record = record
-                new_record['term'] = term
-                if match.start() < len(record['answer'])/4:
-                    excerpt_start = 0
-                else:
-                    excerpt_start = match.start()/2
-                if match.end() < len(record['answer'])*.75:
-                    excerpt_end = len(record['answer'])
-                else:
-                    excerpt_end = (len(record['answer']) - match.end())/2
-                new_record['excerpt'] = record['answer'][excerpt_start:excerpt_end]
-                term_records.append(new_record)
+            matches = re.finditer(rf'{term}', record['answer'])
         else:
-            for match in re.finditer(rf'{term}', record['answer'], re.IGNORECASE):
-                new_record = record
-                new_record['term'] = term
-                if match.start() < len(record['answer'])/4:
-                    excerpt_start = 0
-                else:
-                    excerpt_start = match.start()/2
-                if match.end() < len(record['answer'])*.75:
-                    excerpt_end = len(record['answer'])
-                else:
-                    excerpt_end = (len(record['answer']) - match.end())/2
-                new_record['excerpt'] = record['answer'][excerpt_start:excerpt_end]
-                term_records.append(new_record)
+            matches = re.finditer(rf'{term}', record['answer'], re.IGNORECASE)
+        for match in matches:
+            new_record = record
+            new_record['term'] = match.group()
+            excerpt_start = 0 if (match.start() - excerpt_range) < 0 else match.start()-excerpt_range
+            excerpt_end = len(record['answer']) if (match.end()+excerpt_range) > len(record['answer']) else match.end()+excerpt_range
+            excerpt = record['answer'][excerpt_start:excerpt_end]
+            new_record['excerpt'] = excerpt
+            term_records.append(new_record)
     return term_records
+
+
+def find_term_locations(record, term, case_senstive=False, excerpt_range=15):
+    if case_senstive:
+        matches = re.finditer(rf'{term}', record['answer'])
+    else:
+        matches = re.finditer(rf'{term}', record['answer'], re.IGNORECASE)
+    term_locations = []
+    for match in matches:
+        term_location = {}
+        term_location['term'] = match.group()
+        term_location['term_index'] = match.start()
+        term_location['term_end_index'] = match.end()
+        excerpt_start = 0 if term_location['term_index'] - excerpt_range < 0 else term_location['term_index'] - excerpt_range
+        excerpt_end = len(record['answer']) if term_location['term_end_index'] + excerpt_range > len(record['answer']) else term_location['term_index'] + excerpt_range
+        term_location['excerpt'] = record["answer"][excerpt_start:excerpt_end]
+        term_location['bn'] = record['bn']
+        term_location['section'] = record['section']
+        term_location['question'] = record['question']
+        term_locations.append(term_location)
+    return term_locations
 
 
 def query1(records, ci_terms, cs_terms, rh_schools):
     for term in ci_terms:
-        records = count_search_term(records, term)
+        records = add_count_term_to_record(records, term)
     for term in cs_terms:
-        records = count_search_term(records, term, True)
-    #this is the first thing to paramterize (filtering by school)
-    filename = "portfolio-schools_search-terms.csv"
+        records = add_count_term_to_record(records, term, True)
+    filename = "portfolio-schools_term-counts.csv"
     #build field names based on what was searched for
     fieldnames = ['bn', 'section', 'question', 'answer']
     for term in ci_terms:
@@ -80,23 +88,49 @@ def query1(records, ci_terms, cs_terms, rh_schools):
         writer.writerows(to_write)
 
 
-def query2(records, cs_terms, ci_terms, rh_schools):
-    for term in ci_terms:
-        ci_term_records = create_new_record_each_term(records, term)
-    for term in cs_terms:
-        cs_term_records = create_new_record_each_term(records, term, True)
-    term_records = ci_term_records + cs_term_records
-    #this is the first thing to paramterize (filtering by school)
-    filename = "portfolio-schools_term_counts.csv"
+def query2(records, terms, school_filter):
+    #a list of dictionaries with term, bn, section, question, excerpt
+    all_term_locations = []
+    for term in terms:
+        for record in records:
+            if record['bn'] in school_filter:
+                one_term_locations = find_term_locations(record, term, True, 50)
+                all_term_locations.extend(one_term_locations)
+    filename = "portfolio-schools_terms-locations_(case_senstive).csv"
     #build field names based on what was searched for
-    fieldnames = ['bn', 'section', 'question', 'answer', 'term', 'excerpt']
+    fieldnames = ['term', 'bn', 'section', 'question', 'excerpt']
     to_write = []
-    for record in term_records:
+    for term_location in all_term_locations:
         record_to_write = {}
-        if record['bn'] in rh_schools:
-            for fieldname in fieldnames:
-                record_to_write[fieldname] = record[fieldname]
-            to_write.append(record_to_write)
+        for fieldname in fieldnames:
+            record_to_write[fieldname] = term_location[fieldname]
+        to_write.append(record_to_write)
+    with open(filename, 'w') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter='|')
+        writer.writeheader()
+        writer.writerows(to_write)
+
+
+def query3(records, cs_terms, rh_schools):
+    for term in cs_terms:
+        records = add_count_term_to_record(records, term, True)
+    filename = "portfolio-schools_blended-literacy-terms.csv"
+    #build field names based on what was searched for
+    fieldnames = ['bn']
+    for term in cs_terms:
+        fieldnames.append(term)
+    to_write = []
+    for school in rh_schools:
+        school_dict = {}
+        school_dict['bn'] = school
+        for record in records:
+            if record['bn'] == school:
+                for term in cs_terms:
+                    if term in school_dict.keys():
+                        school_dict[term] += record[term]
+                    else:
+                        school_dict[term] = record[term]
+        to_write.append(school_dict)
     with open(filename, 'w') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter='|')
         writer.writeheader()
@@ -107,20 +141,69 @@ def main():
     cep_structure_filepath = './cep1819-structure-clean.csv'
     cep_text_filepaths = './cep_txt_utf'
     records = parse_ceps(cep_text_filepaths, cep_structure_filepath)
-    cs_terms = ['ELA', 'CS']
-    ci_terms = ['math',
-                'coding',
-                'computer',
-                'tech',
-                'technology'
-                'literacy',
-                'blended literacy',
-                'computational thinking',
-                'science',
-                'algorithm',
-                'computer science',
-                'computing',
-                'decomposition']
+    # cs_terms = ['ELA', 'CS', 'Go Math', 'EngageNYC', 'NYC Performance Series', 'Harcourt Science', 'Amplify']
+    cs_terms = ['Accelerated Reader 360',
+                'Amplify',
+                'Pearson',
+                'BrainPOP',
+                'CommonLit',
+                'Core Knowledge',
+                'Cortex',
+                'Discovery Ed Tech Book',
+                'EL Education',
+                'Eureka Math',
+                'Flocabulary',
+                'FOSS',
+                'Freckle',
+                'GoFormative',
+                'GoMath',
+                'Google Classroom',
+                'Great Minds Wit and Wisdom',
+                'IDO',
+                'Illuminate',
+                'Imagine Learning',
+                'INET',
+                'iPuzzle',
+                'iReady',
+                'IXL',
+                'Jump Math',
+                'Kahoot',
+                'Khan Academy',
+                'LearnZillion',
+                'Lexia',
+                'Mathletics ',
+                'MobyMax',
+                'myON',
+                'NearPod',
+                'NewsELA',
+                'Next Generation Learning Standards',
+                'Passports',
+                'Pathblazers',
+                'Picture Perfect Science',
+                'Quill',
+                'Quizlet',
+                'RazKids',
+                'Read180',
+                'Reading AZ',
+                'Readorium',
+                'ReadWorks',
+                'ReadyGen',
+                'ReflexMath',
+                'REWARDS',
+                'Rosetta Stone',
+                'Sanford Harmony',
+                'Schoology',
+                'Splash Math',
+                'STAR reading',
+                'Starfalls',
+                'Summit',
+                'Tableau',
+                'TC Reading and Writing Project',
+                'The Writing Revolution (TWR)',
+                'ThinkCERCA',
+                'Versal',
+                'Writing City',
+                'Zearn']
     rh_schools = ['K516',
                   'X086',
                   'Q013',
@@ -175,7 +258,7 @@ def main():
                   'R010',
                   'M188',
                   'K401']
-    query2(records, cs_terms, ci_terms, rh_schools)
+    query3(records, cs_terms, rh_schools)
 
 
 if __name__ == '__main__':
